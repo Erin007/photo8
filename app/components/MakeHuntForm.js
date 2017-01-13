6b4be3810705418c4a70ef58aad4f07df08b7683
Erin Bond <erin@erinebond.com>
2017-01-13 00:32:35 -0800

    New Hunt skeleton form, needs work

--- app/components/MakeHuntForm.js
+++ app/components/MakeHuntForm.js
@@ -5,11 +5,17 @@ import {
   View,
   Text
 } from 'react-native';
-import Button from './common/Button';
 import huntDetails from './ShareHunt';
-import directiveList from './DirectiveList'
+import DirectiveList from './DirectiveList'
+import Button from './common/Button';
+import Input from './common/Input';
+import Card from './common/Card';
+import CardSection from './common/CardSection';
 
 class newHunt extends Component{
+
+  state = { huntName: '', description: '', directives: [], directive: '', error: '', loading: false}
+
   savePressed() {
     console.log('>>> Save Button Pressed!');
     this._toShareHunt();
@@ -29,6 +35,37 @@ class newHunt extends Component{
         <Text style={styles.text}>
            This is where there will be a form for users to make a new hunt!
         </Text>
+
+        <Card>
+          <CardSection>
+            <Input
+              label = "Name:"
+              placeholder = "Explore the Emerald City!"
+              value = {this.state.huntName}
+              onChangeText = {huntName => this.setState({ huntName })}/>
+          </CardSection>
+
+          <CardSection>
+            <Input
+              label = "Description:"
+              placeholder = ""
+              //secureTextEntry
+              value = {this.state.description}
+              onChangeText = {description => this.setState({ description })}/>
+          </CardSection>
+
+          <CardSection>
+            <Input
+              label = "Directive:"
+              placeholder = "your group at the Space Needle"
+              //secureTextEntry
+              value = {this.state.directive}
+              //What I need to do here is add the directive to an array of directives and then render the directives already in the array below the form so the user knows what they have already entered
+              //maybe also a counter of how many directives are already attached to the hunt?
+              onChangeText = {directive => this.setState({ directive })}/>
+          </CardSection>
+        </Card>
+
         <Button onPress={this.savePressed.bind(this)}> Save </Button>
       </View>
     );
